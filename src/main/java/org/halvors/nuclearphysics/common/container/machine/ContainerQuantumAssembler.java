67eeae2e383a21134fb7d1f00ad7d391ab810a91
kmecpp <kmecpp@gmail.com>
2018-09-15 23:44:30 -0400

    Prevent putting non dark matter items in the wrong QA slots

--- src/main/java/org/halvors/nuclearphysics/common/container/machine/ContainerQuantumAssembler.java
+++ src/main/java/org/halvors/nuclearphysics/common/container/machine/ContainerQuantumAssembler.java
@@ -1,26 +1,50 @@
 package org.halvors.nuclearphysics.common.container.machine;
 
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import org.halvors.nuclearphysics.common.container.ContainerBase;
+import org.halvors.nuclearphysics.common.init.ModItems;
 import org.halvors.nuclearphysics.common.tile.machine.TileQuantumAssembler;
 
 public class ContainerQuantumAssembler extends ContainerBase<TileQuantumAssembler> {
-    public ContainerQuantumAssembler(final InventoryPlayer inventoryPlayer, final TileQuantumAssembler tile) {
-        super(7, inventoryPlayer, tile);
 
-        yInventoryDisplacement = 148;
-        yHotBarDisplacement = 206;
+	public static class DarkMatterSlot extends Slot {
 
-        addSlotToContainer(new Slot(tile, 0, 80, 40));
-        addSlotToContainer(new Slot(tile, 1, 53, 56));
-        addSlotToContainer(new Slot(tile, 2, 107, 56));
-        addSlotToContainer(new Slot(tile, 3, 53, 88));
-        addSlotToContainer(new Slot(tile, 4, 107, 88));
-        addSlotToContainer(new Slot(tile, 5, 80, 103));
-        addSlotToContainer(new Slot(tile, 6, 80, 72));
+		public DarkMatterSlot(IInventory inventory, int slot, int xPos, int yPos) {
+			super(inventory, slot, xPos, yPos);
+		}
 
-        // Player inventory
-        addPlayerInventory(inventoryPlayer.player);
-    }
+		@Override
+		public boolean isItemValid(ItemStack stack) {
+			return stack.getItem() == ModItems.itemDarkMatterCell;
+		}
+	}
+
+	public ContainerQuantumAssembler(final InventoryPlayer inventoryPlayer, final TileQuantumAssembler tile) {
+		super(7, inventoryPlayer, tile);
+
+		yInventoryDisplacement = 148;
+		yHotBarDisplacement = 206;
+		addSlotToContainer(new DarkMatterSlot(tile, 0, 80, 40));
+		addSlotToContainer(new DarkMatterSlot(tile, 1, 53, 56));
+		addSlotToContainer(new DarkMatterSlot(tile, 2, 107, 56));
+		addSlotToContainer(new DarkMatterSlot(tile, 3, 53, 88));
+		addSlotToContainer(new DarkMatterSlot(tile, 4, 107, 88));
+		addSlotToContainer(new DarkMatterSlot(tile, 5, 80, 103));
+		addSlotToContainer(new Slot(tile, 6, 80, 72));
+
+		// Player inventory
+		addPlayerInventory(inventoryPlayer.player);
+	}
+
+	@Override
+	protected boolean mergeItemStack(ItemStack itemStack, int min, int max, boolean negative) {
+		if (itemStack.getItem() == ModItems.itemDarkMatterCell && min <= 6) {
+			return false;
+		}
+		return super.mergeItemStack(itemStack, min, max, negative);
+	}
+
 }
